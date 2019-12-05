24b30ebfc872f34c9fef5e72a87b187d812f7307
manodya <manodyaabeysinghe7@gmail.com>
2019-12-05 11:44:35 +0530

    add all files

---
+++ LoginRegistrationDemo/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using LoginRegistrationDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginRegistrationDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        MVCTutorialEntities db = new MVCTutorialEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult SaveData(SiteUser model)
+        {
+            db.SiteUsers.Add(model);
+            db.SaveChanges();
+            return Json("Registration Successfull", JsonRequestBehavior.AllowGet);
+        }
+    }
+}
